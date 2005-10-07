ada1576a81926666952d026ebdb0ce0163467398
deusx <devnull@localhost>
2005-10-07 04:14:24 +0000

    [svn r631] FeedSpool: Now force_poll uses the 'feed_should_scan' hook to vote yes on scanning

--- docs/eg/force_poll.py
+++ docs/eg/force_poll.py
@@ -1,11 +1,10 @@
-""" """
+"""This is a simpleminded plugin which always votes to force a feed scan."""
+
 from feedspool import config
 from feedspool.plugins import Plugin
 
 class ForcePollPlugin(Plugin):
 
-    ISO_NEVER = '1970-01-01T00:00:00+00:00'
-
-    def feed_scan_start(self, subscription):
-        subscription.meta.set('scan', 'next_poll', self.ISO_NEVER)
+    def feed_should_scan(self, subscription):
+        return True
 
